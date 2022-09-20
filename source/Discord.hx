9ab42b3aca4dd92f82f70865a60bd67d45b651a6
Default Name <95196453+Default-name123@users.noreply.github.com>
2022-09-20 17:24:36 -0500

    a

--- source/Discord.hx
+++ source/Discord.hx
@@ -29,12 +29,14 @@ class DiscordClient
 		}
 
 		DiscordRpc.shutdown();
+		        #end
 	}
 
 	public static function shutdown()
 	{
 				#if desktop		   
 		DiscordRpc.shutdown();
+				#end
 	}
 
 	static function onReady()
@@ -46,6 +48,7 @@ class DiscordClient
 			largeImageKey: 'icon',
 			largeImageText: "Tails Gets Trolled"
 		});
+				#end
 	}
 
 	static function onError(_code:Int, _message:String)
@@ -60,15 +63,18 @@ class DiscordClient
 
 	public static function initialize()
 	{
+				 #if desktop		   
 		var DiscordDaemon = sys.thread.Thread.create(() ->
 		{
 			new DiscordClient();
 		});
 		trace("Discord Client initialized");
+					#end  
 	}
 
 	public static function changePresence(details:String, state:Null<String>, ?smallImageKey : String, ?hasStartTimestamp : Bool, ?endTimestamp: Float)
 	{
+					#if desktop
 		var startTimestamp:Float = if(hasStartTimestamp) Date.now().getTime() else 0;
 
 		if (endTimestamp > 0)
@@ -86,6 +92,7 @@ class DiscordClient
 			startTimestamp : Std.int(startTimestamp / 1000),
             endTimestamp : Std.int(endTimestamp / 1000)
 		});
+				#end
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
