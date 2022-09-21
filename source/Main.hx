ea95357024670cb9f979fd86fc779f275a73d76d
MasterX <95196453+Default-name123@users.noreply.github.com>
2022-09-20 20:11:08 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -82,8 +82,9 @@ class Main extends Sprite
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		SUtil.doTheCheck()				   
-		
+		#if !mobile
 		addChild(new ui.FPSMem(10, 3, 0xFFFFFF));
+                #end
 		
 	}
 
