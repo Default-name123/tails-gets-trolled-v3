672f52721cd93367f6a6917a3f769c9728d080c4
MasterX <95196453+Default-name123@users.noreply.github.com>
2022-09-22 12:51:03 -0500

    update modchart.lua

--- assets/songs/no-heroes/modchart.lua
+++ assets/songs/no-heroes/modchart.lua
@@ -1,4 +1,4 @@
-local tweenObj = initModule("assets/songs/no-heroes/tween.lua")
+local tweenObj = initModule("songs:assets/songs/no-heroes/tween.lua")
 local tweens = {}
 
 function tween(obj,properties,time,style)
