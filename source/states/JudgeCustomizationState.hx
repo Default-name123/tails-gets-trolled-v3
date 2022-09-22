8364ff9e87bc8fd328b09d58a58f56f25f85526d
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-09-22 19:28:09 +0300

    Update JudgeCustomizationState.hx

--- source/states/JudgeCustomizationState.hx
+++ source/states/JudgeCustomizationState.hx
@@ -80,7 +80,7 @@ class JudgeCustomizationState extends MusicBeatState {
     instructions.screenCenter(X);
     add(instructions);
 
-
+    addVirtualPad(NONE, A_B_X_Y);
   }
 
   var prevComboNums:Array<String> = [];
@@ -157,8 +157,8 @@ class JudgeCustomizationState extends MusicBeatState {
     var deltaY = mouseY-FlxG.mouse.screenY;
     mouseX = FlxG.mouse.screenX;
     mouseY = FlxG.mouse.screenY;
-    if(FlxG.keys.justPressed.ESCAPE || FlxG.keys.justPressed.ENTER){
-      if(FlxG.keys.justPressed.ENTER){
+    if(controls.BACK || controls.ACCEPT){
+      if(controls.ACCEPT){
         EngineData.options.judgeX = judgePlacementPos.x;
         EngineData.options.judgeY = judgePlacementPos.y;
         OptionUtils.saveOptions(OptionUtils.options);
@@ -175,14 +175,14 @@ class JudgeCustomizationState extends MusicBeatState {
       draggingJudge=false;
     }
 
-    if(FlxG.keys.justPressed.R){
+    if(controls.RESET){
       judgePlacementPos.set(0,0);
     }
 
     judge.x = defaultPos.x + judgePlacementPos.x;
     judge.y = defaultPos.y + judgePlacementPos.y;
 
-    if(FlxG.keys.justPressed.C){
+    if(controls.PAUSE){
       showCombo();
     }
 
